d63277ddc7e488b32a72e10cd00f91704fd2b7d2
erufailo <jrin2113@gmail.com>
2018-01-21 19:06:15 +0200

    Added right-click and drag to erase feature

--- README.md
+++ README.md
@@ -2,12 +2,22 @@
 
 Final Project for the Google Front end scholarship challenge
 
+Demo: https://codepen.io/JohnRin/pen/PEmgGG
+
 ## Instructions
 
 Your users should be able to:
 
-*Dynamically set the size of the table as an N by M grid.
-*Choose a color.
-*Click a cell in the grid to fill that cell with the chosen color.
+* Dynamically set the size of the table as an N by M grid.
+* Choose a color.
+* Click a cell in the grid to fill that cell with the chosen color.
 
 Your primary task is to implement the makeGrid() function, that dynamically creates a grid that the user can interact with.
+
+## Extra Features
+
+* Click and drag to paint
+* Right-Click and drag to erase
+
+
+
--- designs.js
+++ designs.js
@@ -1,19 +1,27 @@
+/*                  *******Changelog*******
+Live demo: https://codepen.io/JohnRin/pen/PEmgGG
+
+21-1-18: Added right-click to erase feature
+13-1-18: Added min/max values to grid size to prevent lagging in very large canvas
+30-12-17: initial implementation with extra features to drag and paint
+*/
+
 // When size is submitted by the user, call makeGrid()
 function makeGrid(width,height) {
   if($(pixel_canvas).has("tr")){//if pixel_canvas has children, delete them
     $(pixel_canvas).empty();
   }
   //create the grid with the values passed in the function
-  for (var i=0; i < height; i++){
+  for (let i=0; i < height; i++){
     $("#pixel_canvas").append("<tr id=tr"+i+"></tr>");//insert a row in every i iteration
-    for (var j=0; j < width; j++){
+    for (let j=0; j < width; j++){
       $("#pixel_canvas").children("#tr"+i).append("<td></td>");//insert a box in every row equal to the number of width
     }
   }
 }
 $("#sizePicker").submit(function() {//select the form and on submit event
-  var height=( $( "#input_height" ).val() );//take the value of height
-  var width=( $( "#input_width" ).val());//take the value of width
+  let height=( $( "#input_height" ).val() );//take the value of height
+  let width=( $( "#input_width" ).val());//take the value of width
   makeGrid(width,height); // call makeGrid with the dimensions of the grid
   return false; // prevent the form from reloading the page
 });
@@ -24,17 +32,39 @@ $('#pixel_canvas').on('click', 'td', function(){// on the table if a td is click
 /* 
 ****EXTRA FEATURES*******
 */
-//click and drag to paint, for more efficiency
+//click and drag to paint,right-click to erase for more efficiency
 var clicked =false;
-$('#pixel_canvas').on('mousedown', 'td', function(){
-  clicked =true;
+var rightClicked =false;
+$('#pixel_canvas').on('mousedown', 'td', function(e){
+  if(e.which ==1){//if event is click
+    clicked =true;
+    rightClicked=false;
+  }
+  if(e.which==3){//if event is right-click    
+    clicked=false;
+    rightClicked=true;    
+  }
 });
-$('#pixel_canvas').on('mouseup', 'td', function(){
-  clicked =false;
+$('#pixel_canvas').on('mouseup', 'td', function(e){
+  if(e.which ==1){//if event is click
+    clicked =false;
+    rightClicked=false;
+  }
+   if(e.which==3){//if event is right-click
+    rightClicked=false;
+    clicked =false;    
+  }
 });
 
 $('#pixel_canvas').on('mouseenter', 'td', function(){
   if(clicked){ // only if the mouse is down paint the "pixel"
     $(this).css("background-color",$("#colorPicker").val());
-  }
+  }else if(rightClicked){//else erase
+     $(this).css("background-color","white");
+   }
+
+});
+//prevent right-click to show the menu in the canvas
+$('#pixel_canvas').contextmenu(function() {
+  return false;
 });
