2aa9f3f82c6ca9fd72f1804eee3ba040d281e373
erufailo <jrin2113@gmail.com>
2018-01-29 00:09:15 +0200

    Reduced the use of the JQuery selector with variables for better speed, added erase with single right click

--- designs.js
+++ designs.js
@@ -1,5 +1,7 @@
 /*                  *******Changelog*******
 Live demo: https://codepen.io/JohnRin/pen/PEmgGG
+28-1-18: Reduced the use of selector $() with the use of variables
+         Added erase with single right click.
 24-1-18: Fixed Bugs where the paint continued if the cursor went
          out of canvas, and the drag painting started from the next
          "pixel"
@@ -7,72 +9,75 @@ Live demo: https://codepen.io/JohnRin/pen/PEmgGG
 13-1-18: Added min/max values to grid size to prevent lagging in very large canvas
 30-12-17: initial implementation with extra features to drag and paint
 */
+const canvas = $('#pixel_canvas');
+const colorPicker = $("#colorPicker");
 
 // When size is submitted by the user, call makeGrid()
-function makeGrid(width,height) {
-  if($(pixel_canvas).has("tr")){//if pixel_canvas has children, delete them
-    $(pixel_canvas).empty();
+function makeGrid(width, height) {
+  if (canvas.has("tr")) {//if pixel_canvas has children, delete them
+    canvas.empty();
   }
   //create the grid with the values passed in the function
-  for (let i=0; i < height; i++){
-    $("#pixel_canvas").append("<tr id=tr"+i+"></tr>");//insert a row in every i iteration
-    for (let j=0; j < width; j++){
-      $("#pixel_canvas").children("#tr"+i).append("<td></td>");//insert a box in every row equal to the number of width
+  for (let i = 0; i < height; i++) {
+    canvas.append("<tr id=tr" + i + "></tr>");//insert a row in every i iteration
+    for (let j = 0; j < width; j++) {
+      canvas.children("#tr" + i).append("<td></td>");//insert a box in every row equal to the number of width
     }
   }
 }
-$("#sizePicker").submit(function() {//select the form and on submit event
-  let height=( $( "#input_height" ).val() );//take the value of height
-  let width=( $( "#input_width" ).val());//take the value of width
-  makeGrid(width,height); // call makeGrid with the dimensions of the grid
+$("#sizePicker").submit(function () {//select the form and on submit event
+  let height = ($("#input_height").val());//take the value of height
+  let width = ($("#input_width").val());//take the value of width
+  makeGrid(width, height); // call makeGrid with the dimensions of the grid
   return false; // prevent the form from reloading the page
 });
-$('#pixel_canvas').on('click', 'td', function(){// on the table if a td is clicked
-  $(this).css("background-color",$("#colorPicker").val()) // take the color of the colorpicker and paint the backround of the td
+canvas.on('click', 'td', function () {// on the table if a td is clicked
+  $(this).css("background-color", colorPicker.val()) // take the color of the colorpicker and paint the backround of the td
 });
 
 /* 
 ****EXTRA FEATURES*******
 */
 //click and drag to paint,right-click to erase for more efficiency
-var clicked =false;
-var rightClicked =false;
-$('#pixel_canvas').on('mousedown', 'td', function(e){
-  if(e.which ==1){//if event is click
-    clicked =true;
-    rightClicked=false;
+var clicked = false;
+var rightClicked = false;
+canvas.on('mousedown', 'td', function (e) {
+  if (e.which == 1) {//if event is click
+    clicked = true;
+    rightClicked = false;
   }
-  if(e.which==3){//if event is right-click    
-    clicked=false;
-    rightClicked=true;    
+  if (e.which == 3) {//if event is right-click    
+    clicked = false;
+    rightClicked = true;
   }
   e.preventDefault();
 });
-$('#pixel_canvas').on('mouseup', 'td', function(e){
-  if(e.which ==1){//if event is click
-    clicked =false;
-    rightClicked=false;
+canvas.on('mouseup', 'td', function (e) {
+  if (e.which == 1) {//if event is click
+    clicked = false;
+    rightClicked = false;
   }
-   if(e.which==3){//if event is right-click
-    rightClicked=false;
-    clicked =false;    
+  if (e.which == 3) {//if event is right-click
+    rightClicked = false;
+    clicked = false;
   }
-  
-});
-$('#pixel_canvas').on('mouseleave', function(){// if the mouse leaves the canvas reset the variables
-  clicked=false;
-  rightClicked=false;
 
 });
-$('#pixel_canvas').on('mouseenter mousemove', 'td', function(){
-  if(clicked){ // only if the mouse is down paint the "pixel"
-    $(this).css("background-color",$("#colorPicker").val());
-  }else if(rightClicked){//else erase
-     $(this).css("background-color","white");
-   }
+canvas.on('mouseleave', function () {// if the mouse leaves the canvas reset the variables
+  clicked = false;
+  rightClicked = false;
+
+});
+canvas.on('mouseenter mousemove', 'td', function () {
+  if (clicked) { // only if the mouse is down paint the "pixel"
+    $(this).css("background-color",colorPicker.val());
+  } else if (rightClicked) {//else erase
+    $(this).css("background-color", "white");
+  }
 
 });
 //prevent right-click to show the menu in the canvas
-$('#pixel_canvas').contextmenu(function() {
+canvas.contextmenu(function () {
   return false;
+  $(this).css("background-color", "white");// single right click to erase a pixel
 });
